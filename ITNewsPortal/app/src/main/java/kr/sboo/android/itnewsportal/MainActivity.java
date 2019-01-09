03dd1b1a8bb232efa4ae7a27fc5c9f00ea7ba188
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2019-01-09 14:34:19 +0900

    add Test Ad

--- ITNewsPortal/app/src/main/java/kr/sboo/android/itnewsportal/MainActivity.java
+++ ITNewsPortal/app/src/main/java/kr/sboo/android/itnewsportal/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 import butterknife.BindView;
@@ -14,10 +16,12 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.go_to_today_news_button) Button mGoToTodayNewsButton;
     @BindView(R.id.go_to_my_news_button) Button mGoToMyNewsButton;
+    @BindView(R.id.adView) AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MobileAds.initialize(this, "ca-app-pub-1831729606651519~5963495121");
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
@@ -37,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        MobileAds.initialize(this, "ca-app-pub-1831729606651519~5963495121");
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
